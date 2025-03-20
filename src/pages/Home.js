7b3607303e6e0f0dbf1472569f2e097f4555203f
Norberto <norbertoj54@hotmail.com>
2025-03-20 13:05:24 -0300

    Custom grid structure for projects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,6 +18,23 @@ const Home = () => {
                     <Project description={"Proyecto 10"} />
                     <Project description={"Proyecto 11"} />
                     <Project description={"Proyecto 12"} />
+                    <Project description={"Proyecto 13"} />
+                    <Project description={"Proyecto 14"} />
+                    <Project description={"Proyecto 15"} />
+                    <Project description={"Proyecto 16"} />
+                    <Project description={"Proyecto 17"} />
+                    <Project description={"Proyecto 18"} />
+                    <Project description={"Proyecto 19"} />
+                    <Project description={"Proyecto 20"} />
+                    <Project description={"Proyecto 21"} />
+                    <Project description={"Proyecto 22"} />
+                    <Project description={"Proyecto 23"} />
+                    <Project description={"Proyecto 24"} />
+                    <Project description={"Proyecto 25"} />
+                    <Project description={"Proyecto 26"} />
+                    <Project description={"Proyecto 27"} />
+                    <Project description={"Proyecto 28"} />
+                    <Project description={"Proyecto 29"} />
                 </ul>
             </nav>
         </div>
