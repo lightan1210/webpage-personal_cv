ab54cb72637d6eb48d9e47cb3da425c6d3b0d551
Norberto <norbertoj54@hotmail.com>
2025-03-21 00:18:01 -0300

    Details (image and description) on every project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,13 @@
-const Project = ({ description }) => {
+import image from '../images/logo512.png'
+
+const Project = ({ title }) => {
     return (
         <li className="project">
-            <h1>{description}</h1>
+            <h1>{title}</h1>
+            <div className="info">
+                <img className="preview" src={image} />
+                <p className="epigraphy">Officia amet exercitation consectetur mollit et nostrud ut dolor aliqua non sunt deserunt. Velit qui et ad amet aliqua cupidatat. Officia amet minim ea nisi minim cillum in dolore. Duis eu ex ex qui magna. Sit aliqua fugiat est et nisi voluptate Lorem. Commodo qui voluptate et nulla qui ipsum elit. Laborum elit dolor cillum proident incididunt.</p>
+            </div>
         </li>
     )
 }
